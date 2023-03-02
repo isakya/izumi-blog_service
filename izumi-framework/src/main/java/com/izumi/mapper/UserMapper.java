9e9017ad7bd61f42bd796daed7bc6e0b6c5ddc4b
isakya <1016361268@qq.com>
2023-03-02 19:13:56 +0800

    登陆接口准备工作

---
+++ izumi-framework/src/main/java/com/izumi/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.izumi.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.izumi.domain.entity.User;
+
+
+/**
+ * 用户表(User)表数据库访问层
+ *
+ * @author makejava
+ * @since 2023-03-02 19:05:43
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
