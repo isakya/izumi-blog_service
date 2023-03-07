45dd504d63d6ab47f8bc5fcd65501cd6f870da5c
isakya <1016361268@qq.com>
2023-03-07 08:33:35 +0800

    统一异常处理

--- izumi-blog/src/main/java/com/izumi/controller/BlogLoginController.java
+++ izumi-blog/src/main/java/com/izumi/controller/BlogLoginController.java
@@ -2,8 +2,11 @@ package com.izumi.controller;
 
 import com.izumi.domain.ResponseResult;
 import com.izumi.domain.entity.User;
+import com.izumi.enums.AppHttpCodeEnum;
+import com.izumi.exception.SystemException;
 import com.izumi.service.BlogLoginService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +18,10 @@ public class BlogLoginController {
 
     @PostMapping("/login")
     public ResponseResult login(@RequestBody User user) {
+        if(!StringUtils.hasText(user.getUserName())) {
+            // 提示必须要传用户名
+            throw new SystemException(AppHttpCodeEnum.REQUIRE_USERNAME);
+        }
         return blogLoginService.login(user);
     }
 }
---
+++ izumi-framework/src/main/java/com/izumi/handler/exception/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.izumi.handler.exception;
+
+import com.izumi.domain.ResponseResult;
+import com.izumi.enums.AppHttpCodeEnum;
+import com.izumi.exception.SystemException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(SystemException.class)
+    public ResponseResult systemExceptionHandler(SystemException e){
+        //打印异常信息
+        log.error("出现了异常！ {}",e);
+        //从异常对象中获取提示信息封装返回
+        return ResponseResult.errorResult(e.getCode(),e.getMsg());
+    }
+
+
+    @ExceptionHandler(Exception.class)
+    public ResponseResult exceptionHandler(Exception e){
+        //打印异常信息
+        log.error("出现了异常！ {}",e);
+        //从异常对象中获取提示信息封装返回
+        return ResponseResult.errorResult(AppHttpCodeEnum.SYSTEM_ERROR.getCode(),e.getMessage());
+    }
+}
