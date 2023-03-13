75105c19f0b67d512e64b7ddd9439b21f17f7cc1
isakya <1016361268@qq.com>
2023-03-13 15:14:35 +0800

    个人信息查询接口功能实现

--- izumi-framework/src/main/java/com/izumi/service/UserService.java
+++ izumi-framework/src/main/java/com/izumi/service/UserService.java
@@ -1,6 +1,7 @@
 package com.izumi.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.izumi.domain.ResponseResult;
 import com.izumi.domain.entity.User;
 
 
@@ -12,4 +13,5 @@ import com.izumi.domain.entity.User;
  */
 public interface UserService extends IService<User> {
 
+    ResponseResult userInfo();
 }
--- izumi-framework/src/main/java/com/izumi/service/impl/UserServiceImpl.java
+++ izumi-framework/src/main/java/com/izumi/service/impl/UserServiceImpl.java
@@ -1,9 +1,13 @@
 package com.izumi.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.izumi.domain.ResponseResult;
 import com.izumi.domain.entity.User;
+import com.izumi.domain.vo.UserInfoVo;
 import com.izumi.mapper.UserMapper;
 import com.izumi.service.UserService;
+import com.izumi.utils.BeanCopyUtils;
+import com.izumi.utils.SecurityUtils;
 import org.springframework.stereotype.Service;
 
 /**
@@ -15,4 +19,14 @@ import org.springframework.stereotype.Service;
 @Service("userService")
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
 
+    @Override
+    public ResponseResult userInfo() {
+        // 获取当前用户id
+        Long userId = SecurityUtils.getUserId();
+        // 根据用户id查询用户信息
+        User user = getById(userId);
+        // 封装成UserInfoVo
+        UserInfoVo vo = BeanCopyUtils.copyBean(user, UserInfoVo.class);
+        return ResponseResult.okResult(vo);
+    }
 }
