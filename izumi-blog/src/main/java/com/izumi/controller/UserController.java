40ee5e391a5ecb5f2892e44ebf54770a5ca50430
isakya <1016361268@qq.com>
2023-03-19 16:39:33 +0800

    更新个人信息接口功能实现

--- izumi-blog/src/main/java/com/izumi/controller/UserController.java
+++ izumi-blog/src/main/java/com/izumi/controller/UserController.java
@@ -1,11 +1,10 @@
 package com.izumi.controller;
 
 import com.izumi.domain.ResponseResult;
+import com.izumi.domain.entity.User;
 import com.izumi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
@@ -16,5 +15,10 @@ public class UserController {
     @GetMapping("/userInfo")
     public ResponseResult userInfo() {
         return userService.userInfo();
+    }
+
+    @PutMapping("/userInfo")
+    public ResponseResult updateUserInfo(@RequestBody User user) {
+        return userService.updateUserInfo(user);
     }
 }
