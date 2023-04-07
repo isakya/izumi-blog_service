9046455ca25be36c0d99b5014995f9e47192d5dd
isakya <1016361268@qq.com>
2023-04-07 18:19:05 +0800

    注册功能接口实现

--- izumi-framework/src/main/java/com/izumi/service/impl/UserServiceImpl.java
+++ izumi-framework/src/main/java/com/izumi/service/impl/UserServiceImpl.java
@@ -1,14 +1,21 @@
 package com.izumi.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.izumi.domain.ResponseResult;
 import com.izumi.domain.entity.User;
 import com.izumi.domain.vo.UserInfoVo;
+import com.izumi.enums.AppHttpCodeEnum;
+import com.izumi.exception.SystemException;
 import com.izumi.mapper.UserMapper;
 import com.izumi.service.UserService;
 import com.izumi.utils.BeanCopyUtils;
 import com.izumi.utils.SecurityUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestBody;
 
 /**
  * 用户表(User)表服务实现类
@@ -35,5 +42,52 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
         updateById(user);
 
         return null;
+    }
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public ResponseResult register(User user) {
+        // 对数据进行非空判断
+        if(!StringUtils.hasText(user.getUserName())) {
+            throw new SystemException(AppHttpCodeEnum.USERNAME_NOT_NULL);
+        }
+        if(!StringUtils.hasText(user.getPassword())) {
+            throw new SystemException(AppHttpCodeEnum.PASSWORD_NOT_NULL);
+        }
+        if(!StringUtils.hasText(user.getEmail())) {
+            throw new SystemException(AppHttpCodeEnum.EMAIL_NOT_NULL);
+        }
+        if(!StringUtils.hasText(user.getNickName())) {
+            throw new SystemException(AppHttpCodeEnum.NICKNAME_NOT_NULL);
+        }
+        // 对数据进行是否存在的判断
+        if(userNameExist(user.getUserName())) {
+            throw new SystemException(AppHttpCodeEnum.USERNAME_EXIST);
+        }
+        if(nickNameExist(user.getNickName())) {
+            throw new SystemException(AppHttpCodeEnum.NICKNAME_EXIST);
+        }
+        // 对密码进行加密处理
+        String encodePassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodePassword);
+
+        // 存入数据库
+        save(user);
+
+        return ResponseResult.okResult();
+    }
+
+    private boolean nickNameExist(String nickName) {
+        LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(User::getNickName, nickName);
+        return count(queryWrapper) > 0;
+    }
+
+    private boolean userNameExist(String userName) {
+        LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(User::getUserName, userName);
+        return count(queryWrapper) > 0;
     }
 }
