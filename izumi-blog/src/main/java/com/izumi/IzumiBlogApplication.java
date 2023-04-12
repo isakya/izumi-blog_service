5c231a6d20fc1bc6acb922a6c396c69f689a9ba6
isakya <1016361268@qq.com>
2023-04-12 16:19:52 +0800

    初始化viewCount的Redis数据

--- izumi-blog/src/main/java/com/izumi/IzumiBlogApplication.java
+++ izumi-blog/src/main/java/com/izumi/IzumiBlogApplication.java
@@ -3,9 +3,11 @@ package com.izumi;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @MapperScan("com.izumi.mapper")
+@EnableScheduling
 public class IzumiBlogApplication {
     public static void main(String[] args) {
         SpringApplication.run(IzumiBlogApplication.class, args);
