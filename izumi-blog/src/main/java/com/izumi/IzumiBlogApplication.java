59e1a78ea43fbfcab8760ce12058aab18f6c6a6c
isakya <1016361268@qq.com>
2023-04-14 03:47:16 +0800

    Swagger2：Controller配置、接口配置、接口参数配置

--- izumi-blog/src/main/java/com/izumi/IzumiBlogApplication.java
+++ izumi-blog/src/main/java/com/izumi/IzumiBlogApplication.java
@@ -4,10 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @MapperScan("com.izumi.mapper")
 @EnableScheduling
+@EnableSwagger2
 public class IzumiBlogApplication {
     public static void main(String[] args) {
         SpringApplication.run(IzumiBlogApplication.class, args);
--- izumi-blog/src/main/java/com/izumi/controller/CommentController.java
+++ izumi-blog/src/main/java/com/izumi/controller/CommentController.java
@@ -4,11 +4,16 @@ import com.izumi.constants.SystemConstants;
 import com.izumi.domain.ResponseResult;
 import com.izumi.domain.entity.Comment;
 import com.izumi.service.CommentService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/comment")
+@Api(tags = "评论", description = "评论相关接口")
 public class CommentController {
     @Autowired
     private CommentService commentService;
@@ -24,6 +29,11 @@ public class CommentController {
     }
 
     @GetMapping("/linkCommentList")
+    @ApiOperation(value = "友链评论", notes = "获取一页友链评论")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "pageNum",value = "页号"),
+            @ApiImplicitParam(name = "pageSize",value = "每页大小")
+    })
     public ResponseResult linkCommentList(Integer pageNum, Integer pageSize) {
         return commentService.commentList(SystemConstants.LINK_COMMENT, null, pageNum, pageSize);
     }
