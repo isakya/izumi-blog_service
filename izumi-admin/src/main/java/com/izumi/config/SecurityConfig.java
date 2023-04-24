ee109731f1baa46ae01d91680d004ea6be414084
isakya <1016361268@qq.com>
2023-04-24 09:57:48 +0800

    除登陆接口外其他接口添加认证控制

--- izumi-admin/src/main/java/com/izumi/config/SecurityConfig.java
+++ izumi-admin/src/main/java/com/izumi/config/SecurityConfig.java
@@ -46,13 +46,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .authorizeRequests()
                 // 对于登录接口 允许匿名访问
-                // .antMatchers("/login").anonymous()
+                .antMatchers("/user/login").anonymous()
                 // 登出必须携带token认证
                 // .antMatchers("/logout").authenticated()
                 // .antMatchers("/user/userInfo").authenticated()
                 // .antMatchers("/upload").authenticated()
                 // 除上面外的所有请求全部不需要认证即可访问
-                .anyRequest().permitAll();
+                .anyRequest().authenticated();
 
         // 配置异常处理器
         http.exceptionHandling()
