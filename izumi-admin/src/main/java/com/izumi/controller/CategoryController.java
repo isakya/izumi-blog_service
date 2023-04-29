d798e318bc44738ac906cbb29ffca29ef0415a40
isakya <1016361268@qq.com>
2023-04-29 20:55:41 +0800

    实现导出分类列表接口

--- izumi-admin/src/main/java/com/izumi/controller/CategoryController.java
+++ izumi-admin/src/main/java/com/izumi/controller/CategoryController.java
@@ -1,13 +1,21 @@
 package com.izumi.controller;
 
+import com.alibaba.excel.EasyExcel;
+import com.alibaba.fastjson.JSON;
 import com.izumi.domain.ResponseResult;
 import com.izumi.domain.entity.Category;
 import com.izumi.domain.vo.CategoryVo;
+import com.izumi.domain.vo.ExcelCategoryVo;
+import com.izumi.enums.AppHttpCodeEnum;
 import com.izumi.service.CategoryService;
+import com.izumi.utils.BeanCopyUtils;
+import com.izumi.utils.WebUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @RestController
@@ -22,4 +30,22 @@ public class CategoryController {
         return ResponseResult.okResult(list);
     }
 
+    @GetMapping("/export")
+    public void export(HttpServletResponse response) {
+        try {
+            // 设置下载文件的请求头
+            WebUtils.setDownLoadHeader("分类.xlsx", response);
+            // 获取需要导出的数据
+            List<Category> categoryVos = categoryService.list();
+            List<ExcelCategoryVo> excelCategoryVos = BeanCopyUtils.copyBeanList(categoryVos, ExcelCategoryVo.class);
+            // 把数据写入到Excel中
+            EasyExcel.write(response.getOutputStream(), ExcelCategoryVo.class).autoCloseStream(Boolean.FALSE).sheet("分类导出")
+                    .doWrite(excelCategoryVos);
+        } catch (Exception e) {
+            e.printStackTrace();
+            // 如果出现异常也要响应json
+            ResponseResult result = ResponseResult.errorResult(AppHttpCodeEnum.SYSTEM_ERROR);
+            WebUtils.renderString(response, JSON.toJSONString(result));
+        }
+    }
 }
